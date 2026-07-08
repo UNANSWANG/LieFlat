5ec3c1854fbbae04d2b81fe29a81a54b45f5abaf
ANSWANG <2444683349@qq.com>
2026-07-08 16:55:46 +0800

    铡刀范围字段

--- assets/scripts/controller/props/sawProps.ts
+++ assets/scripts/controller/props/sawProps.ts
@@ -6,15 +6,15 @@ const { ccclass, property } = _decorator;
 @ccclass('sawProps')
 export class sawProps extends gamePropsBase {
     /**触发的生命阈值 */
-    sawThreshold: number = 0.3;
-    /**造成的百分比伤害 */
-    sawDamage: number = 0.1;
+    sawThreshold: number = 0.1;
+    /**检测范围（格数） */
+    sawRange: number = 10;
 
     /**初始化专属数据 */
     initPropsData() {
         super.initPropsData();
         this.sawThreshold = commonConfig.getValueNumber("sawThreshold") / 100;
-        this.sawDamage = commonConfig.getValueNumber("sawDamage") / 100;
+        this.sawRange = commonConfig.getValueNumber("sawRange");
     }
 
     /**道具开始生效 */

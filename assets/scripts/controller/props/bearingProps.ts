51bceea10b73f3940796a4a1327f20e6cc04c1e8
ANSWANG <2444683349@qq.com>
2026-07-14 16:14:02 +0800

    轴承的动画

--- assets/scripts/controller/props/bearingProps.ts
+++ assets/scripts/controller/props/bearingProps.ts
@@ -1,27 +1,63 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, tween, Tween } from 'cc';
 import { gamePropsBase } from './gamePropsBase';
 import { commonConfig } from '../../json/jsonCommon';
 import { tilePropsType } from '../tileItemController';
-const { ccclass, property } = _decorator;
+import { ccTools } from '../../extention/generalTools';
+import { imgPath } from '../../manager/pathConfig';
+const { ccclass } = _decorator;
 @ccclass('bearingProps')
 export class bearingProps extends gamePropsBase {
     /**提高防御塔的百分比攻击速度 */
     bearingSpeed: number = 0.5;
+    /**图2旋转一圈时长 */
+    private rotateDuration: number = 4;
 
     /**初始化专属数据 */
     initPropsData() {
         super.initPropsData();
         this.bearingSpeed = commonConfig.getValueNumber("bearingSpeed") / 100;
     }
+
+    /**初始化道具图片 */
+    initPropsImg() {
+        super.initPropsImg();
+        ccTools.loadImg(this.img2, imgPath.gamePprops + this.propsType);
+    }
     
     /**道具开始生效 */
     startProps() {
-
+        this.playRotateAnim();
     }
 
     /**道具结束生效 */
     endProps() {
         super.endProps();
+        this.stopRotateAnim();
+    }
+
+    /**播放图2循环旋转动画 */
+    private playRotateAnim() {
+        if (!this.img2?.node) {
+            return;
+        }
+
+        Tween.stopAllByTarget(this.img2.node);
+        tween(this.img2.node)
+            .set({ angle: 0 })
+            .to(this.rotateDuration, { angle: -360 })
+            .union()
+            .repeatForever()
+            .start();
+    }
+
+    /**停止图2旋转动画并恢复状态 */
+    private stopRotateAnim() {
+        if (!this.img2?.node) {
+            return;
+        }
+
+        Tween.stopAllByTarget(this.img2.node);
+        this.img2.node.angle = 0;
     }
 
     /** 获取指定房间内轴承带来的攻击速度倍率 */

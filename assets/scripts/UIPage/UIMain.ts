42b9a0f526b2ffbf0d840fcbb3ed9abb228b9d51
ANSWANG <2444683349@qq.com>
2026-07-23 18:03:31 +0800

    替换炮台资源

--- assets/scripts/UIPage/UIMain.ts
+++ assets/scripts/UIPage/UIMain.ts
@@ -102,8 +102,7 @@ export class UIMain extends UIBase {
 
     /**开始游戏 */
     cliskStartBtn() {
-        // uiMgr.openPage(UIPath.UIMatch);
-        uiMgr.playMoneyAnim(this.startBtn, 100);
+        uiMgr.openPage(UIPath.UIMatch);
     }
 
     /**点击设置 */

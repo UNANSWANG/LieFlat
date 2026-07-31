ba448cd0d1de5b93842bd1b5a381e9a9e2b12b5c
ANSWANG <2444683349@qq.com>
2026-07-31 14:08:23 +0800

    开启合批

--- assets/scripts/UIPage/UILoading.ts
+++ assets/scripts/UIPage/UILoading.ts
@@ -225,6 +225,7 @@ export class UILoading extends Component {
         this.isSceneLoading = true;
         this.currentProgressPercent = 1;
         this.refreshProgress();
+        return
         this.scheduleOnce(() => director.loadScene("main"), 0);
     }
 

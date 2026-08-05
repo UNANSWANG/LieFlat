e9787954f41170297d9d28b107585dce497fa534
ANSWANG <2444683349@qq.com>
2026-08-05 13:16:31 +0800

    增加图集配置

--- assets/scripts/UIPage/UILoading.ts
+++ assets/scripts/UIPage/UILoading.ts
@@ -170,7 +170,7 @@ export class UILoading extends Component {
             resolve();
         });
     }
-    loadItems = [UIPath.UIMain, UIPath.UISetting, UIPath.UISkinStore, UIPath.UIStore, UIPath.UIMatch];
+    loadItems = [UIPath.UIMain, UIPath.UISetting, UIPath.UISkinStore, UIPath.UIStore, UIPath.UIMatch, UIPath.UIWarm, UIPath.UIAnnouncement,UIPath.UIMoney];
 
     /**预加载界面 */
     async preLoadPage() {
